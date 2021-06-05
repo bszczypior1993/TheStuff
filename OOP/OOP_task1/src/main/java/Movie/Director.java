e2e69efd9910b4b8a3a1fc6866a24c92180f1e43
bszczypior1993 <bszczypior@interia.pl>
2021-06-05 18:33:59 +0200

    movie class task first commit

---
+++ OOP/OOP_task1/src/main/java/Movie/Director.java
@@ -0,0 +1,12 @@
+package Movie;
+
+public class Director {
+    String name;
+    String surname;
+
+    public Director(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+}
---
+++ OOP/OOP_task1/src/main/java/Movie/Movie.java
@@ -0,0 +1,15 @@
+package Movie;
+
+public class Movie {
+String title;
+Director director;
+int lengthMinutes;
+int yearOfProduction;
+public Movie (String title, Director director, int lengthMinutes, int yearOfProduction){
+    this.title = title;
+    this.director = director;
+    this.lengthMinutes = lengthMinutes;
+    this. yearOfProduction = yearOfProduction;
+}
+
+}
