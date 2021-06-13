7c4f51d0acaf0d36e1d75be2ba5afd39d63e8a6c
bszczypior1993 <bszczypior@interia.pl>
2021-06-14 00:41:59 +0200

    Triangle toString task first commit

---
+++ OOP/OOP_TriangleClass/src/main/java/TriangleClass.java
@@ -0,0 +1,12 @@
+public class TriangleClass {
+    public int width;
+    public int height;
+
+    @Override
+    public String toString() {
+        return "TriangleClass{" +
+                "width=" + width +
+                ", height=" + height +
+                '}';
+    }
+}
