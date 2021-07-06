68acb99de46c54b0008bd5c8b7dc3f09ee92271a
bszczypior1993 <bszczypior@interia.pl>
2021-07-06 09:46:29 +0200

    Refactoring 2 task Bicycle not inheriting

---
+++ CleanCode/java-introduction-master/src/main/java/pl/futurecollars/lesson5/task5/Bicycle.java
@@ -0,0 +1,7 @@
+package pl.futurecollars.lesson5.task5;
+
+public class Bicycle {
+    protected String noEngineStatement() {
+        return "Bicycle does not have an engine";
+    }
+}
---
+++ CleanCode/java-introduction-master/src/main/java/pl/futurecollars/lesson5/task5/Car.java
@@ -0,0 +1,4 @@
+package pl.futurecollars.lesson5.task5;
+
+public class Car extends Vehicle {
+}
