54358567a8e2278115ad9c1a3013a56d6e14c400
bszczypior1993 <bszczypior@interia.pl>
2021-08-07 14:53:42 +0200

    Names list task using Set for duplicate removal

--- JavaCollections1/NameList/src/main/java/NameList.java
+++ JavaCollections1/NameList/src/main/java/NameList.java
@@ -5,23 +5,13 @@ public class NameList {
     public static void main(String[] args) {
 
         ArrayList<java.lang.String> names = new ArrayList<>(Arrays.asList("Anna", "Katarzyna", "Tymon", "Tadeusz", "Anna", "Karolina", "Dorota", "Piotr"));
-        ArrayList<java.lang.String> newListNoDuplicates = removeDuplicates(names);
+        Set<String> namesNoDuplicates = new LinkedHashSet<>(names);;
         ArrayList<java.lang.String> newListBackwards = arrangeBackwards(names);
         ArrayList<java.lang.String> newListSwappedNames = swapNames(names);
-        System.out.println("Lista bez duplikatów: " + newListNoDuplicates);
+        System.out.println("Lista bez duplikatów: " + namesNoDuplicates);
         System.out.println("Lista od końca: " + newListBackwards);
         System.out.println("Elementy po zmianie : Anna -> Joanna: " + newListSwappedNames);
 
-    }
-
-    public static <String> ArrayList<java.lang.String> removeDuplicates(ArrayList<java.lang.String> namesNoDuplicates) {
-        ArrayList<java.lang.String> newListNoDuplicates = new ArrayList<>();
-        for (java.lang.String element : namesNoDuplicates) {
-            if (!newListNoDuplicates.contains(element)) {
-                newListNoDuplicates.add(element);
-            }
-        }
-        return newListNoDuplicates;
     }
 
     public static <String> ArrayList<java.lang.String> arrangeBackwards(ArrayList<java.lang.String> namesBackwards) {
