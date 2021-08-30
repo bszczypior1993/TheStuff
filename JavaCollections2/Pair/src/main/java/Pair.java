989845339de555db85b15a96e68d1afea39a48ee
bszczypior1993 <bszczypior@interia.pl>
2021-08-31 00:16:22 +0200

    pair class attempt two

--- JavaCollections2/Pair/src/main/java/Pair.java
+++ JavaCollections2/Pair/src/main/java/Pair.java
@@ -1,39 +1,21 @@
-import java.util.LinkedHashMap;
-
 public class Pair<K, V> {
 
-    public static void main(String[] args) {
-        Pair pair = new Pair();
-        pair.add("A", "B");
-        pair.add(1, 2);
-        pair.add("A1", "B2");
-        pair.printElement(KeyOrValue.VALUES);
+    private K k;
+    private V v;
 
+    public void addK(K kAdd) {
+        k = kAdd;
     }
 
-    LinkedHashMap<K, V> pairList = new LinkedHashMap<>();
-
-    enum KeyOrValue {
-        KEYS,
-        VALUES
+    public void addV(V vAdd) {
+        v = vAdd;
     }
 
-    public void add(K k, V v) {
-        pairList.put(k, v);
+    public void getK() {
+        System.out.println(k);
     }
 
-    public void printElement(KeyOrValue keyOrValue) {
-        switch (keyOrValue) {
-            case KEYS:
-                System.out.println(pairList.keySet());
-                break;
-
-            case VALUES:
-                System.out.println(pairList.values());
-                break;
-        }
-
-
+    public void getV() {
+        System.out.println(v);
     }
-
 }
